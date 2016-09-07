d2e1ea0674010069b8e834f959fbb9e25214d65a
Ioannis Charalampidis <johnys2@gmail.com>
2016-09-07 13:19:47 +0200

    Proper handling of the id attributes

--- src/js/pages/services/ServicesTab.js
+++ src/js/pages/services/ServicesTab.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {Link, RouteHandler} from 'react-router';
 import {StoreMixin} from 'mesosphere-shared-reactjs';
 
+import Application from '../../structs/Application';
 import AlertPanel from '../../components/AlertPanel';
 import Breadcrumbs from '../../components/Breadcrumbs';
 import DCOSStore from '../../stores/DCOSStore';
@@ -373,7 +374,7 @@ var ServicesTab = React.createClass({
           parentGroupId={item.getId()}
           onClose={this.handleCloseGroupFormModal}/>
         <ServiceFormModal open={state.isServiceFormModalShown}
-          id={serviceId}
+          service={new Application({id: serviceId})}
           onClose={this.handleCloseServiceFormModal}/>
       </div>
     );
